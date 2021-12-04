1b9e47667183cb0ed6c1c65262781976028ff49f
Bernard Van De Walle <bvandewalle@splunk.com>
2021-12-04 13:15:28 -0700

    Cleaning up

--- 04/main.go
+++ 04/main.go
@@ -27,83 +27,17 @@ func main() {
 	}
 
 	file.Close()
-	part1(input)
-	part2(input)
+	part1(input, false)
+	part1(input, true)
 }
 
-func part1(input []string) {
+func part1(input []string, part2 bool) {
 	allNumbers := []int{}
 	for _, j := range strings.Split(input[0], ",") {
 		ij, _ := strconv.Atoi(j)
 		allNumbers = append(allNumbers, ij)
 	}
 
-	allBoards := [][][]int{}
-
-	currentBoard := [][]int{}
-	for i, iv := range input[2:] {
-		if (i+1)%6 == 0 && i > 0 {
-			allBoards = append(allBoards, currentBoard)
-			currentBoard = [][]int{}
-			continue
-		}
-
-		ii := strings.ReplaceAll(strings.TrimSpace(iv), "  ", " ")
-
-		iline := []int{}
-		for _, j := range strings.Split(ii, " ") {
-			ij, _ := strconv.Atoi(j)
-			iline = append(iline, ij)
-		}
-		currentBoard = append(currentBoard, iline)
-	}
-	allBoards = append(allBoards, currentBoard)
-
-	for _, n := range allNumbers {
-		for _, b := range allBoards {
-			for _, l := range b {
-				for i, iv := range l {
-					if iv == n {
-						l[i] = -1
-					}
-				}
-			}
-		}
-
-		for _, b := range allBoards {
-		outer1:
-			for _, l := range b {
-				for _, iv := range l {
-					if iv != -1 {
-						continue outer1
-					}
-				}
-
-				fmt.Println(calculateResult(b, n))
-				return
-			}
-		outer2:
-			for i := 0; i < len(b[0]); i++ {
-				for j := 0; j < len(b); j++ {
-					if b[j][i] != -1 {
-						continue outer2
-					}
-				}
-
-				fmt.Println(calculateResult(b, n))
-				return
-			}
-
-		}
-	}
-}
-
-func part2(input []string) {
-	allNumbers := []int{}
-	for _, j := range strings.Split(input[0], ",") {
-		ij, _ := strconv.Atoi(j)
-		allNumbers = append(allNumbers, ij)
-	}
 	allBoards := [][][]int{}
 
 	currentBoard := [][]int{}
@@ -128,7 +62,6 @@ func part2(input []string) {
 	mem := map[int]bool{}
 
 	for _, n := range allNumbers {
-
 		for _, b := range allBoards {
 			for _, l := range b {
 				for i, iv := range l {
@@ -149,10 +82,16 @@ func part2(input []string) {
 				}
 
 				mem[in] = true
-				if len(mem) == len(allBoards) {
+				if part2 {
+					if len(mem) == len(allBoards) {
+						fmt.Println(calculateResult(b, n))
+						return
+					}
+				} else {
 					fmt.Println(calculateResult(b, n))
 					return
 				}
+
 			}
 		outer2:
 			for i := 0; i < len(b[0]); i++ {
@@ -163,7 +102,12 @@ func part2(input []string) {
 				}
 
 				mem[in] = true
-				if len(mem) == len(allBoards) {
+				if part2 {
+					if len(mem) == len(allBoards) {
+						fmt.Println(calculateResult(b, n))
+						return
+					}
+				} else {
 					fmt.Println(calculateResult(b, n))
 					return
 				}
